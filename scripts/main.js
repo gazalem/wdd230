9c0ddb3fc374e6782d4cd862c11398c5c49e59c1
MONTOYA, Alan <mon21033@byui.edu>
2023-01-05 14:59:08 -0500

    Rewrite index html, styles and JS

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,7 @@
-// save current date time in a variable
-const date = new Date();
-// Use querySelecto to obtain html element
-let currentYear = document.querySelector("#year");
+// Use querySelector to obtain html element
+const currentYear = document.querySelector("#year");
 // insert in HTML element the year portion of date
-currentYear.textContent = date.getFullYear();
+currentYear.textContent = new Date().getFullYear();
 
 // options for locale time format of dates
 const options = {
@@ -12,6 +10,7 @@ const options = {
   month: "long",
   day: "numeric",
 };
+
 // last modified of document
 let lastModification = new Date(document.lastModified);
 let lastModifiedId = document.querySelector("#lastmodified");
