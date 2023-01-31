53250134d4ad66bd14e3916053c1757e1016bd7c
MONTOYA, Alan <mon21033@byui.edu>
2023-01-31 12:12:49 -0500

    DOM Manipulation Assigment W05

---
+++ dommanipulation/scripts/app.js
@@ -0,0 +1,26 @@
+const addChapterBtn = document.querySelector("button");
+
+addChapterBtn.addEventListener("click", () => {
+    const favChapter = document.querySelector("#favchap");
+    const listOfChapters = document.querySelector("#list");
+    const listItem = document.createElement("li");
+
+    // Create Delete button of the item
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = `<i class="fa-solid fa-trash"></i>`;
+    deleteBtn.id = "delete";
+    
+    // insert values to the list Item
+    listItem.append(favChapter.value, deleteBtn);
+
+    // Insert list item to the list
+    listOfChapters.appendChild(listItem);
+
+    // clean input
+    favChapter.value = "";
+
+    // create functionality to delete item
+    deleteBtn.addEventListener("click", () => {
+        listOfChapters.removeChild(listItem);
+    });
+});
