20d5da3b77f3b9ca6992b878516df75a0bbd4d4c
MONTOYA, Alan <mon21033@byui.edu>
2023-02-28 02:49:50 -0500

    Latter-Day Prophets fetch data - Setup

---
+++ lesson09/scripts/prophets.js
@@ -0,0 +1,43 @@
+const dataURL = "https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json";
+
+async function getProphetData() {
+    const response = await fetch(dataURL);
+    const data = await response.json();
+    // We reference the prophet array of the data object
+    // given the structure of the json file
+    // show json data in table format
+    // console.table(data.prophets);
+    displayProphets(data.prophets);
+}
+
+const displayProphets = (prophets) => {
+    // Select the output container element
+    const cards = document.querySelector("div.cards");
+
+    prophets.forEach(prophet => {
+        // Create elements to add into the div.cards element
+        let card = document.createElement("section");
+        let h2 = document.createElement("h2");
+        let portrait = document.createElement("img");
+
+        // Build the h2 content out to show the prophet's full name
+        h2.textContent = `${prophet.name} ${prophet.lastname}`;
+
+        // Build the image out to show the prophet's portrait
+        // and relevant attributes to the image element
+        portrait.src = `${prophet.imageurl}`;
+        portrait.alt = `Portrait of ${prophet.name} ${prophet.lastname}`;
+        portrait.width = "340";
+        portrait.height = "440";
+        portrait.setAttribute("loading", "lazy");
+
+        // Append the section(card) with the created elements
+        card.appendChild(h2);
+        card.appendChild(portrait);
+        
+        // Append to the div.cards element
+        cards.appendChild(card);
+    });
+};
+
+getProphetData();
